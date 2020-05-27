f237e7d850ae7a5168f62a5ce06c73b3be0c83bd
robertmedinasb <robertmedib1@gmail.co>
2020-05-26 22:07:49 -0500

    Using counter_cache for the field review_count

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,18 +1,6 @@
 # frozen_string_literal: true
 
 class Review < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, counter_cache: :review_count
   belongs_to :reviewable, polymorphic: true
-
-  after_create do |review|
-    user = User.find(review.user_id)
-    user.increment(:review_count)
-    user.save
-  end
-
-  after_destroy do |review|
-    user = User.find(review.user_id)
-    user.decrement(:review_count)
-    user.save
-  end
 end
