1d39f935ea401b6a200a5d903e94bee601e81cba
robertmedinasb <robertmedib1@gmail.co>
2020-05-28 01:25:23 -0500

    Adding index,new and show views and forms por Company

---
+++ app/controllers/companies_controller.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+class CompaniesController < ApplicationController
+  def index
+    @companies = Company.all
+      end
+
+  def new
+    @company = Company.new
+  end
+
+  def create
+    @company = Company.new(company_params)
+    if @company.save
+      redirect_to company_path(@company)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @company = Company.find(params[:id])
+  end
+
+  def update; end
+
+  private
+
+  def company_params
+    params.require(:company).permit(:name, :description, :start_date, :country)
+  end
+end
